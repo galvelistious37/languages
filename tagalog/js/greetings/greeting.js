fe3f58cfd6a6c057d493fc888c4ba9efbc980c97
johnny <john.robertson88@yahoo.com>
2023-08-11 21:49:49 -0700

    Move greeting values into a separate js file for separation of duties. Move js and css files into js and css folders

---
+++ tagalog/js/greetings/greeting.js
@@ -0,0 +1,16 @@
+
+function getTagalogGreetings(){
+    let dictionary = new Map();
+    dictionary.set("Pagbati", "Greetings");
+    dictionary.set("Mabuhay!", "Welcome!");
+    dictionary.set("Kumusta?", "Hi/Hello/How are you?");
+    dictionary.set("Ayos lang. Ikaw?", "I'm doing fine. How about you?");
+    dictionary.set("Ako si [pangalan].", "I am [name].");
+    dictionary.set("Ako (naman) si [pangalan]", "I am (also/likewise/as well) [name]");
+    dictionary.set("Ang pangalan ko ay [pangalan].", "My name is [name].");
+    dictionary.set("Anong pangalan mo?", "What is your name?");
+    dictionary.set("Ikinagagalak [rin] kitang makilala.", "Nice to meet you [too].");
+    return dictionary;
+}
+
+getTagalogGreetings();
